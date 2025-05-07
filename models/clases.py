382bf8da0ae5497f864c720d54bf2867ea756b3b
gague-UAM <ggpaiz@uamv.edu.ni>
2025-05-06 21:26:05 -0600

    Clase de la Pila

---
+++ models/clases.py
@@ -0,0 +1,19 @@
+class Pila:
+    def __init__(self):
+        self.donantes = []
+    
+    def push(self, donante):
+        self.donantes.append(donante)
+    
+    def pop(self):
+        if not self.esta_vacia():
+            return self.donantes.pop()
+        return None
+    
+    def peek(self):
+        if not self.esta_vacia():
+            return self.donantes[-1]
+        return None
+    
+    def esta_vacia(self):
+        return len(self.donantes) == 0
