3e215c4c4270e151c65ca746d0e08b2d21e405ba
gague-UAM <ggpaiz@uamv.edu.ni>
2025-05-06 21:21:04 -0600

    Ejercicio grupo 3

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+Grupo 3.
+
+En una campaña de donación de sangre en un hospital de Estelí, los datos de los donantes se almacenan en una pila según el orden en que se procesan. Si ocurre un problema técnico, se debe revertir el último registro. Implementa un sistema para registrar donantes (push), eliminar el último (pop), y mostrar el donante actual en proceso.
+
+"""
